c30bc080fc4ebb46a72548f18a098c49b70f7ee0
Syafli Shovin <syaflishovin@gmail.com>
2023-04-28 10:06:39 +0700

    Initial Commit

---
+++ MC1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MC1
+//
+//  Created by Syafli Shovin on 28/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
