313db54c148ed791fee4264335f83a84c1a196b3
Raymond <metekohy233@gmail.com>
2023-04-29 08:01:50 +0700

    Initial Commit

---
+++ Mini1/Mini1App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini1App.swift
+//  Mini1
+//
+//  Created by Jeremy Raymond on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
