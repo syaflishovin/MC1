4289986aea793a438098395d1154fb6f3db58799
Syafli Shovin <syaflishovin@gmail.com>
2023-05-04 17:07:25 +0700

    initial commit

---
+++ Mini1/Features/Mini1/Presenters/Views/CategoryCounterView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCounterView.swift
+//  Mini1
+//
+//  Created by Syafli Shovin on 04/05/23.
+//
+
+import SwiftUI
+
+struct CategoryCounterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryCounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryCounterView()
+    }
+}
